780630ded194c0ee6c91a3fc117e345ad28c43bf
NaqibKhusairy <naaqibkhusairy@gmail.com>
2024-02-13 12:16:37 +0800

    Initial commit

---
+++ app/src/main/java/com/naqib/logo_splash_screens/Splash.java
@@ -0,0 +1,34 @@
+package com.naqib.logo_splash_screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.Space;
+
+public class Splash extends AppCompatActivity {
+
+    private static int SPLASH_TIMEOUT = 4470;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView imageView = findViewById(R.id.splash_image);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.splash_animation);
+        imageView.startAnimation(animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
+    }
+}
